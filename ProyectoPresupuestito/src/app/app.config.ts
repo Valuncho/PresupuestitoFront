98dfd08e320c92493f86848063cd7ae23dea9046
Juan <bravo43juancho@gmail.com>
2024-08-22 20:30:02 -0300

    beta 3.1 Fix rutes

--- ProyectoPresupuestito/src/app/app.config.ts
+++ ProyectoPresupuestito/src/app/app.config.ts
@@ -4,19 +4,17 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideStore } from '@ngrx/store';
-import { ClientsReducer, clientsStateFeatureKey } from './modules/clients/state';
+
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { provideEffects, EffectsModule } from '@ngrx/effects';
-import {ClientEffect} from "./modules/clients/state/index";
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes), provideAnimationsAsync(),
     provideRouter(routes, withComponentInputBinding()),
-    provideStore({
-       'clientsState' : ClientsReducer
-    }),
+    
 
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
 ]
