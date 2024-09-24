f2b6bd8cb7cfef1d77cf21627a35a00b5925c882
Juan <bravo43juancho@gmail.com>
2024-09-24 09:07:50 -0300

    Model fix

--- ProyectoPresupuestito/src/app/core/services/supplier.service.ts
+++ ProyectoPresupuestito/src/app/core/services/supplier.service.ts
@@ -18,6 +18,7 @@ export class SupplierService {
   private suppliersHistory : SupplierHistory [] = [
     {
       idSupplierHistory: 1,
+      invoices : [],
       oSupplier: {
         idSupplier: 1001,
         note: "hola",
@@ -35,6 +36,7 @@ export class SupplierService {
     },
     {
       idSupplierHistory: 2,
+      invoices : [],
       oSupplier: {
         idSupplier: 1002,
         note: "felix",
