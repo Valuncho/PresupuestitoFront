3b6366744f5b7dab5fb8bb4da5af056095b191e3
felix <felixagustinbengoa@gmail.com>
2024-10-03 18:52:24 -0300

    "minor changes"

--- ProyectoPresupuestito/src/app/modules/employee/pages/employee-Details/employee-Details.component.ts
+++ ProyectoPresupuestito/src/app/modules/employee/pages/employee-Details/employee-Details.component.ts
@@ -7,11 +7,16 @@ import { Salary } from '../../../../core/model/Salary';
 import { EmployeeComponent } from '../../components/employee/employee.component';
 import { EmployeeListComponent } from '../../components/employee-list/employee-list.component';
 import { EmployeeCardComponent } from '../../components/employeeCard/employeeCard.component';
+import { SalaryListComponent } from '../../../salary/components/salary-list/salary-list.component';
+import { ModalService } from '../../../../core/utils/modal.service';
+import { Payment } from '../../../../core/model/Payment';
+import { PaymentsFormComponent } from '../../../payments/components/payments-form/payments-form.component';
+import { SalaryFormComponent } from '../../../salary/components/salary-form/salary-form.component';
 
 @Component({
     selector: 'app-employee-details',
     standalone: true,
-    imports: [CommonModule, EmployeeComponent,EmployeeListComponent,EmployeeCardComponent],
+    imports: [CommonModule, EmployeeComponent,SalaryListComponent,EmployeeCardComponent],
     templateUrl: './employee-Details.component.html',
     styleUrl: './employee-Details.component.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
@@ -21,6 +26,7 @@ export class EmployeeDetailsComponent {
     private router = inject(Router);
     private activatedRoute = inject(ActivatedRoute);
     private employeeService = inject(EmployeeService);
+    private modalService = inject(ModalService)
     id : number  = 0;
     currentEmployee! : Employee;
     employee = signal<Employee | undefined>(undefined);
@@ -30,10 +36,11 @@ export class EmployeeDetailsComponent {
         this.id = parseInt(this.activatedRoute.snapshot.params['employeeId']);
     }
 
+    //Botones
     goToSalaryForm(){
-
+        this.modalService.openModal<SalaryFormComponent,Salary>(SalaryFormComponent);
     }
     openPaymentForm(){
-
+        this.modalService.openModal<PaymentsFormComponent,Payment>(PaymentsFormComponent);
     }
 }
