9422b1479155b472783427218118328e954fa80a
Juan <bravo43juancho@gmail.com>
2024-10-24 15:48:14 -0300

    fix minorchanges

--- ProyectoPresupuestito/src/app/modules/materials/pages/material-form-view/material-form-view.component.ts
+++ ProyectoPresupuestito/src/app/modules/materials/pages/material-form-view/material-form-view.component.ts
@@ -1,12 +1,10 @@
-import { booleanAttribute, Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { CategoryListComponent } from '../../components/lists/category-list/category-list.component';
 import { SubcategoryFormComponent } from "../../components/forms/subcategory-form/subcategory-form.component";
 import { CategoryFormComponent } from "../../components/forms/category-form/category-form.component";
 import { MaterialFormComponent } from "../../components/forms/material-form/material-form.component";
 import { SubcategoryListComponent } from "../../components/lists/subcategory-list/subcategory-list.component";
 
-import { MaterialControllerService } from '../../../../core/controllers/material-controller.service';
-import { Action } from 'rxjs/internal/scheduler/Action';
 
 @Component({
   selector: 'app-material-form-view',
@@ -16,22 +14,5 @@ import { Action } from 'rxjs/internal/scheduler/Action';
   styleUrl: './material-form-view.component.css'
 })
 export class MaterialFormViewComponent {
-
- 
-
-private MaterialController = inject(MaterialControllerService);
-
-ngOnInit(): void {
-  // this.MaterialController.setEditMode(false)
-  // this.MaterialController.setCategory(this.MaterialController.getEmptyCategory())
-  // this.MaterialController.setSubcategory(this.MaterialController.getEmptySubCategory())
-  // this.MaterialController.setMaterial(this.MaterialController.getEmptyMaterial());
-
-  
-}
-
-notice($event: boolean) {
-  throw new Error('Method not implemented.');
-}
 
 }
