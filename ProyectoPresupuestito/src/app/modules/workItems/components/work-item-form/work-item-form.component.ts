4851a3d7b16ac64342358f883b47c927a78385af
Juan <bravo43juancho@gmail.com>
2024-11-04 00:23:23 -0300

    Improve workItemFrom

--- ProyectoPresupuestito/src/app/modules/workItems/components/work-item-form/work-item-form.component.ts
+++ ProyectoPresupuestito/src/app/modules/workItems/components/work-item-form/work-item-form.component.ts
@@ -1,12 +1,10 @@
-import { Component, EventEmitter, inject, Output } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { MaterialControllerService } from '../../../../core/controllers/material-controller.service';
 import { WorkControllerService } from '../../../../core/controllers/work-controller.service';
-import { InvoiceItem } from '../../../../core/model/invoiceItem';
 import { Item } from '../../../../core/model/Item';
 import { Material } from '../../../../core/model/Material';
-import { WorkRequest } from '../../../../core/request/workRequest';
 import { ItemService } from '../../../../core/services/item.service';
 import { ModalService } from '../../../../core/utils/modal.service';
 import { MaterialListComponent } from '../../../materials/components/lists/material-list/material-list.component';
@@ -28,17 +26,14 @@ export class WorkItemFormComponent {
   private itemService = inject(ItemService);
   private workController = inject(WorkControllerService);
   private utils = inject(UtilsService);
-  
-  currentItem : ItemRequest  =  this.materialController.getEmptyItemRequest();  
-  workId : number = 0;
-  currentMaterial : Material = this.materialController.getEmptyMaterial();
-
-  
+  //Suscriptions
   private WorkSubscription: Subscription = new Subscription;
   private ItemSubscription: Subscription = new Subscription;
   private MaterialSubscription: Subscription = new Subscription;
-
-
+  //Properties
+  currentItem : ItemRequest = this.materialController.getEmptyItemRequest();
+  workId : number = 0;
+  currentMaterial : Material = this.materialController.getEmptyMaterial();
   edit : boolean = false;
   itemForm : FormGroup = new FormGroup({
     id : new FormControl(0),
@@ -49,37 +44,23 @@ export class WorkItemFormComponent {
 
 
 ngOnInit(): void {
-  this.edit = this.materialController.getEditMode()
-   this.WorkSubscription = this.workController.getWorkModel().subscribe(res =>{
+  this.edit = this.materialController.getEditMode();
+  this.WorkSubscription = this.workController.getWorkModel().subscribe(res =>{
     this.workId = res.workId;
-    })
-    
-    this.ItemSubscription = this.materialController.getItem().subscribe(res =>{ 
-      this.edit = this.materialController.getEditMode();
-      this.toEdit(res!)
-      
-      
-     })
-
-     this.MaterialSubscription = this.materialController.getMaterial().subscribe(res =>{
-      this.currentMaterial = res!
-      this.setNewMaterial()
-      
-     })
-}
-
-toEdit(item : Item){
-  this.materialController.setMaterial(item.oMaterial)
-  this.itemForm.patchValue({
-    id: item.itemId,
-    material : item.oMaterial.materialName,
-    idmaterial : item.oMaterial.materialId,
-    quantity : item.quantity
   })
-  
+  this.ItemSubscription = this.materialController.getItem().subscribe(res =>{
+    this.edit = this.materialController.getEditMode();
+    if(this.edit) {
+      this.toEdit(res!)
+    }
+  })
+
+  this.MaterialSubscription = this.materialController.getMaterial().subscribe(res =>{
+    this.currentMaterial = res!
+    this.setNewMaterial()
+  })
 }
   ngOnDestroy(): void {
-
     this.materialController.setMaterial(this.materialController.getEmptyMaterial());
     this.materialController.setItem(this.materialController.getEmptyItem());
     this.materialController.setEditMode(false)
@@ -88,44 +69,53 @@ toEdit(item : Item){
     this.ItemSubscription.unsubscribe();
   }
 
-  openMaterialList(){
-    this.modalService.openModal<MaterialListComponent,Material>(MaterialListComponent);
-    
-  }
+toEdit(item : Item){
+    this.materialController.setMaterial(item.oMaterial)
+    this.itemForm.patchValue({
+      id: item.itemId,
+      material : item.oMaterial.materialName,
+      idmaterial : item.oMaterial.materialId,
+      quantity : item.quantity
+    })
+}
 
 
-  sent(){
-    this.itemFormToItem();
-    if(this.edit){
-      this.itemService.putItem(this.currentItem).subscribe({
-          next: ()=>{
-            this.utils.reaload()
-          }
-        });
-    }else{
-      this.itemService.postItem(this.currentItem).subscribe(
-        {
-          next: ()=>{
-            this.utils.reaload()
-          }
-        }
-      );
-    }
-    
-  }
 
-  setNewMaterial(){
-    this.itemForm.patchValue({idmaterial : this.currentMaterial!.materialId, material : this.currentMaterial!.materialName})
-  }
+openMaterialList(){
+  this.modalService.openModal<MaterialListComponent,Material>(MaterialListComponent);
+}
 
-  itemFormToItem(){
-    if(this.itemForm.get("id")?.value == 0){
-      this.currentItem.itemId = undefined;
-    }else{
-      this.currentItem.itemId = this.itemForm.get("id")?.value;
-    }
-    this.currentItem.WorkId = this.workId;
-    this.currentItem.MaterialId = this.currentMaterial.materialId;  
-    this.currentItem.Quantity = this.itemForm.get("quantity")?.value;
+setNewMaterial() {
+  this.itemForm.patchValue({idmaterial: this.currentMaterial!.materialId, material: this.currentMaterial!.materialName})
+}
+
+sent(){
+  this.itemFormToItem();
+  if(this.edit){
+    this.itemService.putItem(this.currentItem).subscribe({
+      next: ()=>{
+        this.utils.reaload()
+      }
+    });
+  }else{
+    this.itemService.postItem(this.currentItem).subscribe({
+      next: ()=>{
+      this.utils.reaload()
+      }
+    });
   }
+}
+
+
+
+itemFormToItem(){
+  if(this.itemForm.get("id")?.value == 0){
+    this.currentItem.itemId = undefined;
+  }else{
+    this.currentItem.itemId = this.itemForm.get("id")?.value;
+  }
+  this.currentItem.WorkId = this.workId;
+  this.currentItem.MaterialId = this.currentMaterial.materialId;
+  this.currentItem.Quantity = this.itemForm.get("quantity")?.value;
+}
 }
