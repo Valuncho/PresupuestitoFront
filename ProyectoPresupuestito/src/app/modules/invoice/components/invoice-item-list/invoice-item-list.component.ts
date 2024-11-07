7340dcf30be4016d2b1000c8d2445f976f7dddd3
felix <felixagustinbengoa@gmail.com>
2024-11-07 17:23:17 -0300

    "add invoice item components"

--- ProyectoPresupuestito/src/app/modules/invoice/components/invoice-item-list/invoice-item-list.component.ts
+++ ProyectoPresupuestito/src/app/modules/invoice/components/invoice-item-list/invoice-item-list.component.ts
@@ -1,21 +1,60 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { TextCardComponent } from "../../../../components/text-card/text-card.component";
-import { Invoice } from '../../../../core/model/Invoice';
-import { Item } from '../../../../core/model/Item';
-import { InvoiceService } from '../../../../core/services/invoice.service';
-import { InvoiceControllerService } from '../../../../core/controllers/invoice-controller.service';
+import { InvoiceItemCardComponent } from '../invoice-item-card/invoice-item-card.component';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmationDialogComponent } from '../../../../components/confirmation-dialog/confirmation-dialog.component';
+import { MaterialControllerService } from '../../../../core/controllers/material-controller.service';
+import { ItemService } from '../../../../core/services/item.service';
+import { ModalService } from '../../../../core/utils/modal.service';
+import { UtilsService } from '../../../../core/utils/utils.service';
+import { InvoiceItem } from '../../../../core/model/invoiceItem';
+import { InvoiceItemFormComponent } from '../invoice-item-form/invoice-item-form.component';
 
 @Component({
   selector: 'app-invoice-item-list',
   standalone: true,
-  imports: [TextCardComponent],
+  imports: [TextCardComponent,InvoiceItemCardComponent],
   templateUrl: './invoice-item-list.component.html',
   styleUrl: './invoice-item-list.component.css'
 })
 export class InvoiceItemListComponent {
-  private invoiceService = inject(InvoiceService);
-  private invoiceController = inject(InvoiceControllerService)
+  //Utils
+  private dialog = inject(MatDialog);
+  private modalService = inject(ModalService);
+  private itemService = inject(ItemService);
+  private materialController = inject(MaterialControllerService);
+  private utils = inject(UtilsService);
+  @Input() items : InvoiceItem[]=[]
+  @Input() options : boolean = false;
+  addItemHandler(){
+    this.modalService.openModal<InvoiceItemFormComponent,InvoiceItem>(InvoiceItemFormComponent);
 
-  invoice : Invoice = this.invoiceController.getEmptyInvoice();
-  options = false;
+  }
+
+  editar($Event : InvoiceItem){
+    this.materialController.setEditMode(true);
+    //this.materialController.setItem($Event);
+    this.modalService.openModal<InvoiceItemFormComponent,InvoiceItem>(InvoiceItemFormComponent);
+  }
+
+  eliminar($Event : InvoiceItem){
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      data: {
+        mensaje: `¿Estás seguro de que deseas eliminar el item de: ${$Event.material.materialName}?`
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.itemService.deleteItem($Event.idInvoiceItem).subscribe(
+          {
+            next: ()=>{
+              this.utils.reaload()
+            }
+          }
+        );
+      }
+    });
+
+  }
 }
